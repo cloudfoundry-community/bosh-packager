f2086baac8d6e5c3fb52ab47d2f81346807534f7
Dr Nic Williams <drnicwilliams@gmail.com>
2013-06-02 17:41:58 -0700

    Initial scaffold for command classes

---
+++ lib/bosh/packager/commands/apply_manifest.rb
@@ -0,0 +1,9 @@
+require "bosh/packager"
+
+module Bosh::Packager::Commands
+  class ApplyManifest
+    def perform
+      
+    end
+  end
+end
---
+++ lib/bosh/packager/commands/target_release.rb
@@ -0,0 +1,10 @@
+require "bosh/packager"
+
+module Bosh::Packager::Commands
+  class TargetRelease
+    attr_accessor :release_name
+    def perform
+      
+    end
+  end
+end
